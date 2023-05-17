c7eec2bae2c27d3b08f15018627de77f67df5f4b
เกษม ศรีเครือดง <redev.rx@gmail.com>
2023-05-17 19:00:19 +0700

    add access data in error body

--- lib/src/client/exception/missing_token.dart
+++ lib/src/client/exception/missing_token.dart
@@ -3,5 +3,5 @@ import 'package:chat_gpt_sdk/src/client/exception/base_error.dart';
 class MissionTokenException extends BaseErrorWrapper {
   @override
   String toString() =>
-      "Not Missing Your Token look more <a>https://beta.openai.com/account/api-keys</a>";
+      "Not Missing Your Token look more https://beta.openai.com/account/api-keys";
 }
