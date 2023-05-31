7778d6dbecec800f0b70d463cbbe7393d33c37e3
เกษม ศรีเครือดง <redev.rx@gmail.com>
2023-05-31 18:10:25 +0700

    modifier

--- lib/src/audio.dart
+++ lib/src/audio.dart
@@ -14,6 +14,7 @@ class Audio {
     void Function(CancelData cancelData)? onCancel,
   }) async {
     final mRequest = await request.toJson();
+
     return _client.postFormData(
       kURL + kTranscription,
       mRequest,
