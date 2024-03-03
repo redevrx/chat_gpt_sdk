c459648b93a3bc3796ea624d4183814d8bc47dc6
Roman Ventskus <romanventskus@cloud.upwork.com>
2024-03-03 20:53:53 +0100

    init

--- lib/src/model/chat_complete/response/message.dart
+++ lib/src/model/chat_complete/response/message.dart
@@ -3,18 +3,21 @@ class Message {
   final String content;
   final String id = "${DateTime.now().millisecondsSinceEpoch}";
   final Map<String, dynamic>? functionCall;
+  final Map<String, dynamic>? toolCalls;
 
-  Message({required this.role, required this.content, this.functionCall});
+  Message({required this.role, required this.content, this.functionCall, this.toolCalls});
 
   factory Message.fromJson(Map<String, dynamic> json) => Message(
         role: json["role"] ?? "",
         content: json["content"] ?? "",
         functionCall: json["function_call"],
+        toolCalls: json["tools_call"],
       );
 
   Map<String, dynamic> toJson() => {
         "role": role,
         "content": content,
         "function_call": functionCall,
+        "tools_call": toolCalls,
       };
 }
