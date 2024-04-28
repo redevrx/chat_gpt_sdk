2bbf39bd2e7ff60941154657cfffc9e7243f26d9
redevrx <redev.rx@gmail.com>
2024-04-28 10:48:14 +0700

    ListRun() may be nullable in some cases #109

--- example_app/openai_app/lib/service/shred_preference/shared_preference.dart
+++ example_app/openai_app/lib/service/shred_preference/shared_preference.dart
@@ -3,7 +3,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 @module
 abstract class SharedPreferenceService {
-  @preResolve
   Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
 }
 
